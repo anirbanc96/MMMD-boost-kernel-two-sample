53c9f7b562290643a1a711cb81aa1bdec17b522a
Anirban Chatterjee <88766573+anirbanc96@users.noreply.github.com>
2023-02-15 17:51:27 -0500

    Update Body-Graph.R

--- Local Alternatives/Code/Graph Based Test/Body-Graph.R
+++ Local Alternatives/Code/Graph Based Test/Body-Graph.R
@@ -2,8 +2,7 @@
 ################# Driver Code for Power over Dimensions ########################
 ################################################################################
 source("Functions.R")
-#------------------------------------------------------------------------------#
-#------------------------------------------------------------------------------#
+
 start <- Sys.time()
 # Number of repetitions
 n.rep <- 1
@@ -17,9 +16,8 @@ p <- 0
 sigma.mult <- 1+seq(0,2,length.out = 11)/sqrt(n)
 # parameter for mean value for H1 distribution
 mu.param <- 0
-#------------------------------------------------------------------------------#
-#------------------------------------------------------------------------------#
-# storing power values for each dimensions
+
+# storing power values for each signal strength
 out.d <- c()
 for (iter in 1:n.rep){
   # storing power values for particular iteration
@@ -47,6 +45,3 @@ power.FR.mat1 <- cbind(sigma.mult,power.FR.mat1)
 
 
 write.csv(power.FR.mat1, file = "Power-FR.csv")
-
-#------------------------------------------------------------------------------#
-#------------------------------------------------------------------------------#
