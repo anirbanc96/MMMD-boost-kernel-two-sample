2a81e093f172a512dcc2143b551824d9f6eb3798
Anirban Chatterjee <88766573+anirbanc96@users.noreply.github.com>
2023-02-16 07:12:08 -0500

    Update Functions.R

--- MNIST-Reduced Contrast and AWGN/Code/Graph Based Tests/Functions.R
+++ MNIST-Reduced Contrast and AWGN/Code/Graph Based Tests/Functions.R
@@ -1,15 +1,16 @@
-#------------------------------------------------------------------------------#
-#---------------------------- FR Test -----------------------------#
-#------------------------------------------------------------------------------#
+########################### FR Test Functions ##################################
 
-############################ Distance Matrix ###################################\
-
-# INPUT : z <- a data matrix (in this case the data matrix of combination of X
-#              and y)
-
-# OUTPUT : A distance matrix corresponding to the above data matrix
+############################ Distance Matrix ###################################
 
 FR.dist <- function(z){
+  
+  ##############################################################################
+  # INPUT: 
+  # z <- a data matrix (in this case the data matrix of combination of X
+  #              and y)
+  # OUTPUT:
+  # A distance matrix corresponding to the above data matrix
+  ##############################################################################
   
   z.dist.mat <- as.matrix(dist(z))
   
@@ -20,11 +21,15 @@ FR.dist <- function(z){
 
 ########################### Counting Duplicates ################################
 
-# INPUT: DF <- A data matrix (in this case the combined data)
-# OUTPUT <- A dataframe with duplicates removed and count of repetition as well
-#           as first occurrence of each data vector
-
 count.dups <- function(DF){
+  
+  ##############################################################################
+  # INPUT: 
+  # DF <- A data matrix (in this case the combined data)
+  # OUTPUT:
+  # A dataframe with duplicates removed and count of repetition as well
+  # as first occurrence of each data vector
+  ##############################################################################
   
   DT <- data.table(DF)
   DT[, list(N= .N, Index=.I[1L]), by = names(DT)]
@@ -35,17 +40,17 @@ count.dups <- function(DF){
 
 ################# Function for conducting the Graph Based Test #################
 
-# INPUT : 
-# m,n <- sample size of two data sets
-# resamp.size <- number of resamples to be done from each data set
-# test.type <- type of graph based test to be done. See gtests documentation
-# for more details.
-# n.iter <- number of iterations to be done for estimating power
-
-# OUTPUT: Estimated power of the Graph based test
-
-
 FR.test <- function(m,n, resamp.size, test.type,n.iter = 1000){
+  
+  ##############################################################################
+  # INPUT : 
+  # m,n <- sample size of two data sets
+  # resamp.size <- number of resamples to be done from each data set
+  # test.type <- type of graph based test to be done. See gtests documentation
+  #              for more details.
+  # n.iter <- number of iterations to be done for estimating power
+  # OUTPUT: Estimated power of the Graph based test
+  ##############################################################################
   
   count <- 0
   
@@ -66,52 +71,48 @@ FR.test <- function(m,n, resamp.size, test.type,n.iter = 1000){
     FRtest <- gTests::g.tests(similarity.mat, X.ID, Y.ID, 
                               test.type = test.type)
     count <- count + (FRtest[1][[1]][2]<0.05)
-    #print (c(i,count))
   }
   return (count/n.iter)
 }
 
-
-#------------------------------------------------------------------------------#
-#---------------------------- Power plot function ----------------------------#
-#------------------------------------------------------------------------------#
+############################ Power of FR Test ##################################
 # Function for comparing power over dimensions
-# INPUTS:
-# n <- no of data points from both distributions
-# sigma.param <- the parameter used for generating variance covariance matrix
-# sigma.mult <- the parameter for multiplying cov matrix under H0 to get cov
-# under H1
-# mu.param <- the parameter for generating mean under H1
-# d.seq <- the vector of dimensions
-# p <- the mixing probability
-# kernel.choice <- choice of kernels under single (first coordinate) and multi
-# kernel setup (second coordinate)
-# OUTPUT:
-# A data frame having powers of single and multiple test under various
-# dimensions
+
 power.d <- function(resamp, set.choice, n.iter = 500){
+  
+  ##############################################################################
+  # INPUTS:
+  # resamp <- number of resamples to be done
+  # set.choice <- vector of sets chosen
+  # n.iter <- number of iterations for estimating power and threshold
+  # OUTPUT:
+  # A data frame having power of FR test for different set choices
+  ##############################################################################
   
   # Libraries for parallelising
   library(foreach)
   library(doParallel)
   
   cores <- detectCores()
-  cl <- makeCluster(cores[1]-1) #not to overload your computer
+  cl <- makeCluster(cores[1]-1)
   
   registerDoParallel(cl)
   
+  # Creating log file for keeping track of iterations
   writeLines(c(""), "log.txt")
   
   out.compare <- foreach(k=1:length(set.choice), .combine=rbind, .export = ls(envir=globalenv())) %dopar% {
     
-    #--------------------------------------------------------------------------#
+    # Loading required libraries
     library(LaplacesDemon)
     library(Rfast)
     library(SpatialPack)
     library(data.table)
     
+    # choice of set for H0 and H1 
     set.x <- set.choice[[k]][1,]; set.y <- set.choice[[k]][2,]
     
+    # getting indices of above set choices
     index.y <- rep(F,length(train.label)); index.x <- rep(F,length(train.label))
     for(i in 1:length(train.label)){
       if (train.label[i] %in% set.y){
@@ -122,26 +123,27 @@ power.d <- function(resamp, set.choice, n.iter = 500){
       }
     }
     
+    # data under H0
     data.X <- train.x[index.x,]
+    # data under H1
     data.Y <- train.x[index.y,]
     
+    # size of data under H0 and H1
     m.x <- dim(data.X)[1]
     n.y <- dim(data.Y)[1]
     
+    # indices under every resamples which is done "resamp" many times
     resamp.x <- matrix(NA, nrow = n.iter, ncol = resamp)
     resamp.y <- matrix(NA, nrow = n.iter, ncol = resamp)
     
     for(i in 1:n.iter){resamp.x[i,] <- sample(1:m.x, resamp, rep = T)}
     for(i in 1:n.iter){resamp.y[i,] <- sample(1:n.y, resamp, rep = T)}
-    #--------------------------------------------------------------------------#
-    # Creating a log file to keep track of progress
+    
+    # Writing in the log file to keep track of progress
     sink("log.txt", append=TRUE)
     cat(paste("Starting iteration",k,"\n"))
-    #--------------------------------------------------------------------------#
     
-    #----------------------------------------------------------------------------#
-    #----------------------------------------------------------------------------#
-    
+    # Estimating power under FR test
     out.row.col1 <- FR.test(m.x,n.y,resamp, test.type = "o", n.iter)
     cat("FR test in iteration",c(out.row.col1,k),"\n")
     
